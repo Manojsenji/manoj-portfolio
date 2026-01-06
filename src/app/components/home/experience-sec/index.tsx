4cd107f647ce677f608cc799d97ecc204fed8396
manoj <manojsreenivas95@gmail.com>
2026-01-06 10:52:54 +0530

    updated certificates

--- src/app/components/home/experience-sec/index.tsx
+++ src/app/components/home/experience-sec/index.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 
 const ExperienceSec = () => {
     const experiences = [
-        {
-            year: "2018-2022",
-            title: "Data Annotator | Annotation Quality Checker | Annotation Team Lead | Project Lead",
-            company: "https://pacecom.co.in/",
-            type: "Fulltime",
-            description: "Led a team of 20+ annotators and reviewers, ensuring high-quality delivery of image and text annotation projects. Managed sprint planning, stand-ups, reporting, and client escalations. Delivered 10+ high-volume projects with >98% accuracy, consistently ahead of deadlines. Ensured quality and adherence to guidelines across multiple project cycles. Trained and mentored junior annotators and facilitated reviewer onboarding programs. "
-        },
-        {
-            year: "2022-present",
-            title: "QA Automation and Manual Engineer",
-            company: "https://pacecom.co.in/",
-            type: "Fulltime",
-            description: " I have a proven track record of identifying critical issues, ensuring product quality, and working closely with developers, product owners, and stakeholders in Agile environments.My expertise includes test case design, automation script development(Selenium + Java), API testing(Postman), and continuous improvement of test processes.I take pride in delivering robust and reliable software through diligent testing and effective collaboration."
-        }
+      {
+        year: "2018-2022",
+        title:
+          "Data Annotator | Annotation Quality Checker | Annotation Team Lead | Project Lead",
+        company: "Pacecom Technologies Pvt Ltd", //https://pacecom.co.in/
+        type: "Fulltime",
+        description:
+          "Led a team of 20+ annotators and reviewers, ensuring high-quality delivery of image and text annotation projects. Managed sprint planning, stand-ups, reporting, and client escalations. Delivered 10+ high-volume projects with >98% accuracy, consistently ahead of deadlines. Ensured quality and adherence to guidelines across multiple project cycles. Trained and mentored junior annotators and facilitated reviewer onboarding programs. ",
+      },
+      {
+        year: "2022-present",
+        title: "QA Automation and Manual Engineer",
+        company: "Pacecom Technologies Pvt Ltd", //https://pacecom.co.in/
+        type: "Fulltime",
+        description:
+          " I have a proven track record of identifying critical issues, ensuring product quality, and working closely with developers, product owners, and stakeholders in Agile environments.My expertise includes test case design, automation script development(Selenium + Java), API testing(Postman), and continuous improvement of test processes.I take pride in delivering robust and reliable software through diligent testing and effective collaboration.",
+      },
     ];
 
     return (
