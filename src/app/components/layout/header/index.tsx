6fe9c97c8ce959ce613b499f189c32d31d5d04c5
manoj <manojsreenivas95@gmail.com>
2026-07-10 12:58:09 +0530

    Updated view resume button

--- src/app/components/layout/header/index.tsx
+++ src/app/components/layout/header/index.tsx
@@ -19,6 +19,16 @@ const Header = () => {
               <button className="relative overflow-hidden cursor-pointer w-fit py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group">
                 <a
                   href="/resume/Manoj S.pdf" // path to your PDF in the public folder
+                  target="_blank"
+                  rel="noopener noreferrer" // file name for download
+                  className="relative z-10 text-xl font-medium text-black group-hover:text-white transition-colors duration-300"
+                >
+                  View Resume
+                </a>
+              </button>
+              <button className="relative overflow-hidden cursor-pointer w-fit py-2 sm:py-3 md:py-5 px-4 sm:px-5 md:px-7 border border-primary rounded-full group">
+                <a
+                  href="/resume/Manoj S.pdf" // path to your PDF in the public folder
                   download="Manoj_Resume.pdf" // file name for download
                   className="relative z-10 text-xl font-medium text-black group-hover:text-white transition-colors duration-300"
                 >
